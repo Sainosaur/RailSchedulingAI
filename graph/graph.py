4e6b6017d2b02f5053395d116690eb47e40d00f3
Sai Adiraju <saiadi4002@gmail.com>
2026-04-01 17:31:48 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -27,58 +27,67 @@ class Segment:
     speed_limit: int | None
 
 
-graph = net.Graph()
+def graph():
+    graph = net.Graph()
 
-# Stations
-chabowka = Station(
-    "Chabówka", 0, 0.582, get_station_elevation("Dworzec Chabówka PKP"), True
-)
-rabka_zdroj = Station(
-    "Rabka-Zdrój", 1, 5.481, get_station_elevation("Dworzec Rabka-Zdrój PKP"), False
-)
-mszana_dolna = Station(
-    "Mszana Dolna", 2, 14.951, get_station_elevation("Przebudowa Mszana Dolna"), True
-)
-tymbark = Station(
-    "Tymbark", 3, 37.160, get_station_elevation("Dworzec PKP Tymbark"), False
-)
-limanowa = Station(
-    "Limanowa", 4, 47.017, get_station_elevation("Dworzec Limanowa PKP"), True
-)
-marcinkowice = Station(
-    "Marcinkowice", 5, 67.394, get_station_elevation("Dworzec Marcinkowice PKP"), False
-)
-nowy_sacz = Station(
-    "Nowy Sącz", 6, 76.651, get_station_elevation("Dworzec Nowy Sącz PKP"), True
-)
+    # Stations
+    chabowka = Station(
+        "Chabówka", 0, 0.582, get_station_elevation("Dworzec Chabówka PKP"), True
+    )
+    rabka_zdroj = Station(
+        "Rabka-Zdrój", 1, 5.481, get_station_elevation("Dworzec Rabka-Zdrój PKP"), False
+    )
+    mszana_dolna = Station(
+        "Mszana Dolna",
+        2,
+        14.951,
+        get_station_elevation("Przebudowa Mszana Dolna"),
+        True,
+    )
+    tymbark = Station(
+        "Tymbark", 3, 37.160, get_station_elevation("Dworzec PKP Tymbark"), False
+    )
+    limanowa = Station(
+        "Limanowa", 4, 47.017, get_station_elevation("Dworzec Limanowa PKP"), True
+    )
+    marcinkowice = Station(
+        "Marcinkowice",
+        5,
+        67.394,
+        get_station_elevation("Dworzec Marcinkowice PKP"),
+        False,
+    )
+    nowy_sacz = Station(
+        "Nowy Sącz", 6, 76.651, get_station_elevation("Dworzec Nowy Sącz PKP"), True
+    )
 
+    # Turning stations into an array
+    stations = [
+        chabowka,
+        rabka_zdroj,
+        mszana_dolna,
+        tymbark,
+        limanowa,
+        marcinkowice,
+        nowy_sacz,
+    ]
 
-# Turning stations into an array
-stations = [
-    chabowka,
-    rabka_zdroj,
-    mszana_dolna,
-    tymbark,
-    limanowa,
-    marcinkowice,
-    nowy_sacz,
-]
-
-# Adding stations and Segments to the graph
-for station in stations:
-    graph.add_node(station.name, data=station)
-    limits = [90, 90, 60, 30, 30, 30]
-    if stations.index(station) < len(stations) - 1:
-        next_station = stations[(stations.index(station)) + 1]
-        graph.add_edge(
-            station.name,
-            next_station.name,
-            data=Segment(
-                station,
-                next_station,
-                next_station.distance - station.distance,
-                (next_station.elevation - station.elevation)
-                / (next_station.distance - station.distance),
-                limits[station.position],
-            ),
-        )
+    # Adding stations and Segments to the graph
+    for station in stations:
+        graph.add_node(station.name, data=station)
+        limits = [90, 90, 60, 30, 30, 30]
+        if stations.index(station) < len(stations) - 1:
+            next_station = stations[(stations.index(station)) + 1]
+            graph.add_edge(
+                station.name,
+                next_station.name,
+                data=Segment(
+                    station,
+                    next_station,
+                    next_station.distance - station.distance,
+                    (next_station.elevation - station.elevation)
+                    / (next_station.distance - station.distance),
+                    limits[station.position],
+                ),
+            )
+    return graph
